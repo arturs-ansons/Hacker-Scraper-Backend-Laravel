bab5f1568b46cdb9104e46b7e3d59712a5c0826e
ansonsarturs <65274330+arturs-ansons@users.noreply.github.com>
2024-04-03 13:19:25 +0300

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,38 +12,30 @@ abstract class TestCase extends BaseTestCase
 
     public function testScrapeHackerNewsCommand()
     {
-        // Log: Starting the test
         print_r("Starting the test\n");
 
-        // Log: Running the scraper command
         print_r("Running the scraper command...\n");
         $this->artisan('scrape:hacker-news');
 
-        // Log: Asserting that articles were inserted into the database
         print_r("Asserting that articles were inserted into the database...\n");
         $this->assertDatabaseCount('articles', 30);
         print_r("Articles were inserted into the database.\n");
 
-        // Log: Deleting an article
         print_r("Deleting an article...\n");
         $deletedArticle = DeletedArticle::factory()->create(['title' => 'Deleted Article']);
         print_r("Deleted article: {$deletedArticle->title}\n");
-
-        // Log: Running the scraper command again
+        
         print_r("Running the scraper command again...\n");
         $this->artisan('scrape:hacker-news');
 
-        // Log: Asserting that articles were inserted into the database again
         print_r("Asserting that articles were inserted into the database again...\n");
         $this->assertDatabaseCount('articles', 30);
         print_r("Articles were inserted into the database again.\n");
 
-        // Log: Asserting that the deleted article was not inserted again
         print_r("Asserting that the deleted article was not inserted again...\n");
         $this->assertDatabaseMissing('articles', ['title' => 'Deleted Article']);
         print_r("Deleted article was not inserted again.\n");
 
-        // Log: Test completed
         print_r("Test completed.\n");
     }
 }
